82009a0b5cfb123e6db0453a9f7f3e83361effb7
Jack Tiong <tiongyingkiat@gmail.com>
2021-09-27 18:21:04 +0800

    - Fixed iOS build issue

---
+++ ios/FoodDeliveryApp.swift
@@ -0,0 +1,8 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Jack Tiong on 27/09/2021.
+//
+
+import Foundation
--- screens/Food/FoodDetail.js
+++ screens/Food/FoodDetail.js
@@ -3,24 +3,15 @@ import {
     View,
     Text
 } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps"
 
 const FoodDetail = () => {
-    const initialRegion = {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    }
-
     return (
-        <View style={{ flex: 1, backgroundColor: 'red' }}>
-            <MapView
-                style={{ flex: 1 }}
-                provider={PROVIDER_GOOGLE}
-                initialRegion={initialRegion}
-
-            />
+        <View
+            style={{
+                flex: 1
+            }}
+        >
+            <Text>FoodDetail</Text>
         </View>
     )
 }
